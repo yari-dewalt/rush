ed5622d58ed68d23fb145768f785c60d87b948ae
yari-dewalt <yari.dewalt@gmail.com>
2025-01-15 14:29:37 -0800

    Setup font and support for basic themes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss';
+const { createThemes } = require('tw-colors');
 
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
@@ -7,5 +8,23 @@ export default {
 		extend: {}
 	},
 
-	plugins: []
+	plugins: [
+    createThemes({
+      light: {
+        'primary': 'steelblue',
+        'secondary': 'darkblue',
+        'brand': '#F3F3F3',
+      },
+      dark: {
+        'background': '#23232B',
+        'primary': '#202026',
+        'secondary': '#2F2F3D',
+        'text-primary': '#E0E0E0',
+        'text-secondary': '#A0A0A0',
+      },
+      dracula: {
+
+      },
+    })
+  ]
 } satisfies Config;
