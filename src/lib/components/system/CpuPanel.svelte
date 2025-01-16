5bffc4c90cb727a062c3567436f6fefe715fb366
yari-dewalt <yari.dewalt@gmail.com>
2025-01-15 19:56:19 -0800

    Create cpu panel with view of bars

---
+++ src/lib/components/system/CpuPanel.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import Panel from "./Panel.svelte";
+  import CpuUsageBars from "./CpuUsageBars.svelte";
+
+  import { faMicrochip } from "@fortawesome/free-solid-svg-icons";
+
+  export let cpuUsages: Array<number>;
+  export let type;
+</script>
+
+<Panel icon={faMicrochip} title="CPU Usage">
+  {#if type === 'bars'}
+    <CpuUsageBars {cpuUsages} />
+  {/if}
+  {#if type === 'pie'}
+    <!-- <CpuUsagePie {cpuUsages} /> -->
+  {/if}
+  {#if type === 'graph'}
+    <!-- <CpuUsageGraph {cpuUsages} /> -->
+  {/if}
+</Panel>
---
+++ src/lib/components/system/CpuUsageBars.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  export let cpuUsages: Array<number>;
+</script>
+
+<div class="grid grid-cols-2 gap-x-4 gap-y-2 text-text-secondary max-h-28 overflow-y-auto">
+  {#each cpuUsages as cpuUsage, i}
+    <div class="flex justify-between items-center gap-3 w-3/4">
+      <div class="flex items-center gap-1">
+        <p class="text-xs whitespace-nowrap min-w-12">Core {i+1}</p>
+        <div class="rounded h-2 grow min-w-14 w-3/4 bg-secondary">
+          <div
+            class="rounded h-2 bg-sky-500"
+            style="width: {cpuUsage}%">
+          </div>
+        </div>
+      </div>
+      <p class="text-xs font-bold min-w-10 text-right">{cpuUsage.toFixed(1)}%</p>
+    </div>
+  {/each}
+</div>
