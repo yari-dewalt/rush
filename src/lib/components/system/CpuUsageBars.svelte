b4541d6e229e4f0b8262921de6fbac02bf00a11b
yari-dewalt <yari.dewalt@gmail.com>
2025-01-16 13:09:32 -0800

    Fix styling

--- src/lib/components/system/CpuUsageBars.svelte
+++ src/lib/components/system/CpuUsageBars.svelte
@@ -2,11 +2,11 @@
   let { cpuUsages } = $props();
 </script>
 
-<div class="grid grid-cols-2 gap-x-4 gap-y-2 text-text-secondary max-h-28 overflow-y-auto">
+<div class="grid grid-cols-2 gap-x-4 gap-y-1.5 overflow-y-auto">
   {#each cpuUsages as cpuUsage, i}
-    <div class="flex justify-between items-center gap-3 w-3/4">
+    <div class="flex justify-between items-center gap-1">
       <div class="flex items-center gap-1">
-        <p class="text-xs whitespace-nowrap min-w-12">Core {i+1}</p>
+        <p class="text-xs whitespace-nowrap min-w-12 text-text-secondary">Core {i+1}</p>
         <div class="rounded h-2 grow min-w-14 w-3/4 bg-secondary overflow-hidden">
           <div
             class="h-2 bg-accent-primary"
@@ -14,7 +14,7 @@
           </div>
         </div>
       </div>
-      <p class="text-xs font-bold min-w-10 text-right">{cpuUsage.toFixed(1)}%</p>
+      <p class="text-xs font-bold min-w-8 text-right text-text-primary">{cpuUsage.toFixed(0)}%</p>
     </div>
   {/each}
 </div>
