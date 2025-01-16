e631d04cd82944968b9dbce736d2761c798f7185
yari-dewalt <yari.dewalt@gmail.com>
2025-01-15 20:31:45 -0800

    Fix look of usageValue

--- src/lib/components/system/Panel.svelte
+++ src/lib/components/system/Panel.svelte
@@ -5,14 +5,14 @@
   let { icon, title, usageValue } = $props();
 </script>
 
-<div class="rounded-lg flex flex-col gap-1 bg-primary p-3">
+<div class="rounded-lg flex flex-col gap-2 bg-primary p-3">
   <div class="flex justify-between items-center">
     <div class="flex items-center gap-2">
       <Fa {icon} class="text-text-primary"/>
       <h2 class="text-sm text-text-primary">{title}</h2>
     </div>
-    <div class="rounded bg-secondary">
-      {usageValue}
+    <div class="text-xs text-text-primary font-bold rounded-xl bg-secondary pl-2 pr-2 p-0.5">
+      {usageValue.toFixed(1)}%
     </div>
   </div>
   <div class="h-[1px] w-full bg-secondary"></div>
