cbee92dbf60ce6a2f9a6affe47761f3e6cfece20
yari-dewalt <yari.dewalt@gmail.com>
2025-01-15 19:55:45 -0800

    Create structure for system stats and panels

---
+++ src/lib/components/system/Panel.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import Fa from "svelte-fa";
+  import type { IconDefinition } from "@fortawesome/free-solid-svg-icons";
+
+  export let icon: IconDefinition;
+  export let title;
+  export let usageValue;
+</script>
+
+<div class="rounded-lg flex flex-col gap-1 bg-primary p-3">
+  <div class="flex justify-between items-center">
+    <div class="flex items-center gap-2">
+      <Fa {icon} class="text-text-primary"/>
+      <h2 class="text-sm text-text-primary">{title}</h2>
+    </div>
+    <div class="rounded bg-secondary">
+      {usageValue}
+    </div>
+  </div>
+  <div class="h-[1px] w-full bg-secondary"></div>
+  <slot></slot>
+</div>
---
+++ src/lib/components/system/SystemStats.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import CpuPanel from '$lib/components/system/CpuPanel.svelte';
+  
+  export let systemInfo;
+</script>
+
+<div class="flex p-2">
+  <CpuPanel type='bars' cpuUsages={systemInfo.cpu_usage}/>
+</div>
