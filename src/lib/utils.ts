2d3607eb3ca2459fa864000c6e6ceccb2fd226ee
yari-dewalt <yari.dewalt@gmail.com>
2025-01-16 13:00:06 -0800

    Change units

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,7 @@
 export function convertBytes(bytes: number, decimals: number = 2): string {
   if (bytes === 0) return '0 B';
   
-  const units = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+  const units = ['B', 'KB', 'MB', 'GB', 'TB', 'PB'];
   const k = 1000;
   
   const i = Math.floor(Math.log(bytes) / Math.log(k));
