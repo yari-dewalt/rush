0362d4044e8e30d6e08e83913a6b2ec766f94ce7
yari-dewalt <yari.dewalt@gmail.com>
2025-01-16 12:59:34 -0800

    Create utility function to convert bytes

---
+++ src/lib/utils.ts
@@ -0,0 +1,12 @@
+export function convertBytes(bytes: number, decimals: number = 2): string {
+  if (bytes === 0) return '0 B';
+  
+  const units = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+  const k = 1000;
+  
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+  
+  const value = bytes / Math.pow(k, i);
+  
+  return `${value.toFixed(decimals)} ${units[i]}`;
+}
