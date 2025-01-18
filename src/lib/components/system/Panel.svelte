febf828c7bd8895765975a555f4049c9fcd9e07a
yari-dewalt <yari.dewalt@gmail.com>
2025-01-17 21:49:24 -0800

    Add types and Popover to Panel

--- src/lib/components/system/Panel.svelte
+++ src/lib/components/system/Panel.svelte
@@ -1,8 +1,18 @@
 <script lang="ts">
   import Fa from "svelte-fa";
   import type { IconDefinition } from "@fortawesome/free-solid-svg-icons";
+  import type { Snippet } from 'svelte';
+  import { Popover } from 'flowbite-svelte';
 
-  let { icon, title, usageValue } = $props();
+  interface PanelProps {
+    children: Snippet;
+    icon: IconDefinition;
+    title: string;
+    usageValue?: number;
+    hoverInfo?: string;
+  }
+
+  let { children, icon, title, usageValue, hoverInfo }: PanelProps = $props();
 </script>
 
 <div class="rounded-lg flex flex-col gap-2 bg-primary p-3 max-h-full">
@@ -12,11 +22,18 @@
       <h2 class="text-sm text-text-primary">{title}</h2>
     </div>
     {#if usageValue}
-      <div class="text-xs text-text-primary font-bold rounded-xl bg-secondary pl-2 pr-2 p-0.5">
+      <div id="{title}-usagevalue" class="text-xs text-text-primary font-bold rounded-xl bg-secondary pl-2 pr-2 p-0.5">
         {usageValue.toFixed(1)}%
       </div>
     {/if}
   </div>
+  <Popover triggeredBy="#{title}-usagevalue" placement="bottom" class={hoverInfo ? '' : 'opacity-0'}>
+    {#if hoverInfo}
+      <div class="bg-primary outline outline-2 outline-secondary text-xs text-text-primary flex items-center justify-center p-2">
+        <h3>{hoverInfo}</h3>
+      </div>
+    {/if}
+  </Popover>
   <div class="min-h-[1px] w-full bg-secondary"></div>
-  <slot></slot>
+  {@render children?.()}
 </div>
